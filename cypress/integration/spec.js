8aabb063a3402e4831fcdc60f5bb8c58347b3c72
Glenn Baevsky <75226627+gbaevsky@users.noreply.github.com>
2021-06-13 12:03:14 +0300

    install cypress dependencies, created test for visiting home page

---
+++ cypress/integration/spec.js
@@ -0,0 +1,13 @@
+describe('First Test', function () {
+    it('Visits the Home Page', function () {
+        cy.visit('http://the-internet.herokuapp.com')
+    })
+})
+
+/*describe('My First Test', () => {
+    it('finds the content "type"', () => {
+      cy.visit('https://example.cypress.io')
+  
+      cy.contains('type')
+    })
+  })*/
