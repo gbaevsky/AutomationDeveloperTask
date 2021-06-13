2c7fb2a1e7ad1592ea84b6a1673f1cf43a85ab71
Glenn Baevsky <75226627+gbaevsky@users.noreply.github.com>
2021-06-13 16:11:42 +0300

    iframe page complete

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,4 +1,9 @@
-describe('First Test', function () {
+beforeEach(() => {
+    cy.visit('http://the-internet.herokuapp.com')
+    cy.log('Visiting the Home Page')
+  })
+
+describe('Home Page', function () {
     it('Visits the Home Page', function () {
         cy.visit('http://the-internet.herokuapp.com')
     })
@@ -6,19 +11,51 @@ describe('First Test', function () {
 
 describe('Checkboxes Page', function () {
     it('Visit Checkboxes Page and for each box change to the opposite state then validate changes', function () {
-      cy.visit('http://the-internet.herokuapp.com')
-    
       cy.contains('Checkboxes').click()
 
       cy.url().should('eq', 'http://the-internet.herokuapp.com/checkboxes')
 
       cy.contains('checkbox 1')
-      //cy.get('[type="checkbox"]').check('checkbox 1')
-      cy.get('[type="checkbox"]').check()
-      cy.get('[type="checkbox"]').should('be.checked')
+      cy.get('[type="checkbox"]').first().check()
+      //cy.get('[type="checkbox"]').check()
+      cy.get('[type="checkbox"]').first().should('be.checked')
       
       cy.contains('checkbox 2')
-      cy.get('[type="checkbox"]').uncheck()
+      cy.get('[type="checkbox"]').uncheck('value="checkbox 2"]')
       cy.get('[type="checkbox"]').should('be.unchecked')
     })
   })
+
+describe('Basic Auth Page', function () {
+    it('Visit Basic Auth Page and check functionality with correct credentials', function () {
+        cy.contains('Basic Auth (user and pass: admin)').click()
+        
+        cy.visit('http://the-internet.herokuapp.com/basic_auth', {
+            auth: {
+                username: 'admin',
+                password: 'admin'
+            }
+        })
+
+        cy.url().should('eq', 'http://the-internet.herokuapp.com/basic_auth')
+        cy.contains('Congratulations! You must have the proper credentials.')
+        cy.log('login successful')
+
+    })
+})
+
+describe('Frames Page', function () {
+    it('Visits Frames Page, then iFrame Page and writes my name ["Glenn"] inside the text paragraph', function () {
+        cy.contains('Frames').click()
+        cy.url().should('eq', 'http://the-internet.herokuapp.com/frames')
+
+        cy.contains('iFrame').click()
+        cy.url().should('eq', 'http://the-internet.herokuapp.com/iframe')
+        cy.contains('An iFrame containing the TinyMCE WYSIWYG Editor')
+
+        cy.getFrame().clear().type('Glenn')
+        cy.getFrame().contains('Glenn')
+        cy.log('Name correctly written in textbox - passes')
+
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getFrame', function () {
+    return cy.get('iframe')
+        .its('0.contentDocument.body')
+})
